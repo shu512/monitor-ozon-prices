7770af3decbab33fa9631fda2b1c1a99b4745683
Stas Gusev <bot-io@yandex.ru>
2023-11-02 13:21:29 +0700

    different improvements

--- 1.js
+++ 1.js
@@ -1,13 +1,27 @@
-const {Builder, Browser, By, ThenableWebDriver, WebElement} = require('selenium-webdriver');
+const { Builder, Browser, By, ThenableWebDriver, WebElement } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const { Client } = require('pg');
+const fs = require('fs');
+const { DateTime, Duration } = require('luxon');
 
 const LIMIT_TRIES = 20;
+const LIMIT_BATCH = 10000;
 
 const priceXpath = "//div[@data-widget='webPrice']/descendant::span[contains(text(),'₽')]";
 const outOfStockXpath = "//div[@data-widget='webOutOfStock']/h2[contains(text(),'Этот товар закончился')]";
 const notDeliveryXpath = "//div[@data-widget='webOutOfStock']/h2[contains(text(),'не доставляется')]";
 const notExistXpath = "//div[@data-widget='error']";
+const plus18Xpath = "//span[contains(text(), 'Подтвердите возраст')]";
+
+const cloudfareId = 'challenge-running';
+
+const clientOptions = {
+  database: 'ozon_prices',
+  user: 'shu512',
+  host: 'localhost',
+  password: 'shu512',
+  port: 5432
+};
 
 /**
  * @param {string} priceText - e.g. "1 309 ₽"
@@ -29,22 +43,36 @@ function awaitPrice(driver) {
     let elem;
     let i = 0;
     while(true) {
-      elem = await driver.findElements(By.xpath(priceXpath))
+      elem = await driver.findElements(By.xpath(priceXpath));
       if (elem.length > 0) return elem[0];
   
-      elem = await driver.findElements(By.xpath(outOfStockXpath))
+      elem = await driver.findElements(By.xpath(outOfStockXpath));
       if (elem.length > 0) return elem[0];
 
-      elem = await driver.findElements(By.xpath(notDeliveryXpath))
+      elem = await driver.findElements(By.xpath(notDeliveryXpath));
       if (elem.length > 0) return elem[0];
 
-      elem = await driver.findElements(By.xpath(notExistXpath))
+      elem = await driver.findElements(By.xpath(notExistXpath));
       if (elem.length > 0) return elem[0];
+
+      elem = await driver.findElements(By.id(cloudfareId));
+      if (elem.length > 0) {
+        console.log('cloudfare');
+        return elem[0];
+      }
+
+      elem = await driver.findElements(By.xpath(plus18Xpath));
+      if (elem.length > 0) {
+        await driver.manage().addCookie({ name: 'is_adult_confirmed', value: 'true' });
+        await driver.manage().addCookie({ name: 'adult_user_birthdate', value: '2001-11-11' });
+        await driver.navigate().refresh();
+      }
   
       i++;
-      if (i >= LIMIT_TRIES) return false;
-      driver.sleep(100);
-  
+      if (i >= LIMIT_TRIES) {
+        return true;
+      }
+      await driver.sleep(100);
     }
   };
 }
@@ -81,15 +109,15 @@ async function getPriceInfoFromPage(driver, id) {
  * @returns { Promise<{ price: number, productId: number } | null> }
  */
 async function runSearch(idFrom, idTo) {
-  const priceInfos = [];
   
   // const options = new chrome.Options().addArguments('--headless=new');
   const options = new chrome.Options();
 
+  let priceInfos = [];
   let driver;
   let priceInfo;
 
-  for(let id = idFrom; id <= idTo; id++) {
+  for(let id = idTo; id >= idFrom; id--) {
     driver = new Builder()
       .forBrowser(Browser.CHROME)
       .setChromeOptions(options)
@@ -100,59 +128,113 @@ async function runSearch(idFrom, idTo) {
       priceInfo = await getPriceInfoFromPage(driver, id);
       if (priceInfo) priceInfos.push(priceInfo);
 
-      if (priceInfos.length >= 10000) {
-        // todo
+      if (priceInfos.length >= LIMIT_BATCH) {
+        await loadToDb(priceInfos);
+        priceInfos = [];
       }
     } finally {
       await driver.quit();
     }
   }
 
-  console.log(priceInfos);
+  await loadToDb(priceInfos);
+  priceInfos = [];
 }
 
-const clientOptions = {
-  database: 'ozon_prices',
-  user: 'shu512',
-  host: 'localhost',
-  password: 'shu512',
-  port: 5432
-};
+/**
+ * prepare data to insert to db
+ * @param { { price: number, productId: number }[] } priceInfos 
+ * @returns {[
+ *    string,
+ *    number[]   
+ * ]}
+ * returns tuple of 2 elements.
+ * - The first elem is string, e.g. ($1::int, $2::int),($3::int, $4::int),($5::int, $6::int),($7::int, $8::int)
+ * - The second one is array of numbers to insert to db
+ */
+function getQuery(priceInfos) {
+  const args = priceInfos.reduce((arr, cur) => [...arr, cur.productId, cur.price], []);
+  let queryArgs = priceInfos.reduce(
+    (query, cur, index) => query + `($${index * 2 + 1}::int, $${index * 2 + 2}::int),`,
+    ''
+  );
+  queryArgs = queryArgs.slice(0, queryArgs.length - 1);
+  const query = `insert into prices (product_id, price) values ${queryArgs};`;
+  return [query, args];
+}
 
-(async function example() {
+/**
+ * 
+ * @async
+ * @param { { price: number, productId: number }[] } priceInfos 
+ */
+async function loadToDb(priceInfos) {
+  if (priceInfos.length === 0) return;
 
-  // const client = new Client(clientOptions);
-  // await client.connect();
+  const client = new Client(clientOptions);
+  await client.connect();
 
-  // const args = priceInfos.reduce((arr, cur) => [...arr, cur.productId, cur.price], []);
-  // // ($1::int, $2::int),($3::int, $4::int),($5::int, $6::int),($7::int, $8::int)
-  // let queryArgs = priceInfos
-  //   .reduce((query, cur, index) => query + `($${index * 2 + 1}::int, $${index * 2 + 2}::int),`, '')
-  //   .slice(0, queryArgs.length - 1);
-  // const query = `insert into prices (product_id, price) values ${queryArgs};`;
-  // await client.query(query, args)
-
+  const [query, args] = getQuery(priceInfos);
   
+  await client.query(query, args)
+  await client.end()
+}
+
+function calculateSearcherOptions({ from, to ,threads }) {
+  const step = Math.ceil((to - from) / threads);
+  const input = Array.from({ length: threads }).map((_, index) => ({
+    from: step * index + from,
+    to: step * (index + 1) + from - 1
+  }));
+
+  input[input.length - 1].to = to;
+  return input;
+}
+
+function logStarted({ from, to ,threads }) {
+  const date = DateTime.now();
+  const dateHumanized = date.toLocaleString(DateTime.DATETIME_FULL);
+  fs.appendFileSync(
+    'ozon_prices.log',
+    `[${dateHumanized}]: Start scanning.  From: ${from}; to: ${to}; threads: ${threads}\n`
+  );
+}
+
+function logFinished({ from, to ,threads }, startDate) {
+  const date = DateTime.now();
+  const dateHumanized = date.toLocaleString(DateTime.DATETIME_FULL);
+  const duration = date.diff(startDate).seconds;
+  fs.appendFileSync(
+    'ozon_prices.log',
+    `[${dateHumanized}]: Finish scanning. From: ${from}; to: ${to}; threads: ${threads}. Duration: ${duration.toHuman()}\n`
+  );
+}
+
+(async function main() {
+  const input = {
+    from    : 806075000,
+    to      : 806076859,
+    threads : 10,
+  };
+
+  // const input = {
+  //   from    : 806070000,
+  //   to      : 806070000,
+  //   threads : 1,
+  // };
   
+  const searcherOptions = calculateSearcherOptions(input);
+  const startDate = logStarted(input);
+
+  const searchers = searcherOptions.map(params => runSearch(params.from, params.to));
+  await Promise.all(searchers);
+
+  logFinished(input, startDate);
+
+  const client = new Client(clientOptions);
+  await client.connect();
   // await client.query('DELETE FROM prices;', [])
-  // const res = await client.query('SELECT * from prices;', [])
-  // console.log(res.rows)
-  // await client.end()
-
-  const one = runSearch(1101013773, 1101013777);
-  const two = runSearch(1101013760, 1101013765);
-  await Promise.all([one, two]);
-
-
+  const res = await client.query('SELECT product_id, price from prices;', [])
+  console.log(res.rows)
+  await client.end();
 })();
-
-
-
-
-
-
-// await driver.get('https://www.ozon.ru/product/1101013778'); // отсутствует
-// await driver.get('https://www.ozon.ru/product/1101013777'); // скидка
-// await driver.get('https://www.ozon.ru/product/1256931175'); // без скидки
-// await driver.get('https://www.ozon.ru/product/1210967301'); // авто
-
