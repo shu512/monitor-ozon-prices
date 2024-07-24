1c0542f782809673d3febfe1db993752843741e8
Stas Gusev <bot-io@yandex.ru>
2024-07-24 23:11:52 +0700

    fix bug: close file in order to get an access after long time

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,23 +1,51 @@
 const fs = require('fs');
 const { DateTime } = require('luxon');
 
+/**
+ * Appends data to the file and close it
+ * @param {string} filename
+ * @param {string} data
+ */
+function append(filename, data) {
+  let fd;
+  try {
+    fd = fs.openSync(filename, 'a');
+    fs.appendFileSync(fd, data, 'utf8');
+  } catch (err) {
+    console.log('[ERROR]: Unable to log to file:', err);
+  } finally {
+    if (fd !== undefined)
+      fs.closeSync(fd);
+  }
+}
+
+/**
+ * Logs the start scanning time to the file
+ * @param {{ from: number, to: number, threads: number}}
+ * @returns {DateTime<true>}
+ */
 function logStarted({ from, to ,threads }) {
-  const filename = process.env.LOG_FILE || ozon_prices.log;
+  const filename = process.env.LOG_FILE || 'ozon_prices.log';
   const date = DateTime.now();
   const dateHumanized = date.toLocaleString(DateTime.DATETIME_FULL);
-  fs.appendFileSync(
+  append(
     filename,
     `[${dateHumanized}]: Start scanning.  From: ${from}; to: ${to}; threads: ${threads}\n`
   );
   return date;
 }
 
+/**
+ * Logs the finish scanning time and duration to the file
+ * @param {{ from: number, to: number, threads: number}}
+ * @returns {DateTime<true>}
+ */
 function logFinished({ from, to ,threads }, startDate) {
-  const filename = process.env.LOG_FILE || ozon_prices.log;
+  const filename = process.env.LOG_FILE || 'ozon_prices.log';
   const date = DateTime.now();
   const dateHumanized = date.toLocaleString(DateTime.DATETIME_FULL);
   const duration = date.diff(startDate);
-  fs.appendFileSync(
+  append(
     filename,
     `[${dateHumanized}]: Finish scanning. From: ${from}; to: ${to}; threads: ${threads}. Duration: ${duration.toHuman()}\n`
   );
