f31453ebfcfb03a0df8552d6d47d31bf900473ca
Stas Gusev <bot-io@yandex.ru>
2024-08-07 18:16:14 +0700

    Improved script management via .env

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 const { DateTime } = require('luxon');
 
-const DEFAULT_FILENAME = 'ozon_prices.log';
+const DEFAULT_FILENAME = 'run.log';
+const DEFAULT_FILENAME_ERRORS = 'errors.log';
+const DEFAULT_FILENAME_SEARCH = 'run_search.log';
+
+function logConsole(msg) {
+  if (process.env.CONSOLE_DEBUG?.toLowerCase() === 'true'){
+    console.log(msg);
+  }
+}
 
 /**
  * Appends data to the file and close it
@@ -13,8 +21,10 @@ function append(filename, data) {
   try {
     fd = fs.openSync(filename, 'a');
     fs.appendFileSync(fd, data, 'utf8');
+    logConsole(data);
   } catch (err) {
-    console.log('[ERROR]: Unable to log to file:', err);
+    const msg = '[ERROR]: Unable to log to file: ';
+    logConsole(msg + err);
   } finally {
     if (fd !== undefined)
       fs.closeSync(fd);
@@ -22,38 +32,77 @@ function append(filename, data) {
 }
 
 /**
- * Logs the start scanning time to the file
+ * Logs the error
+ * @param {string} msg
+ * @param {Exception} err
+ */
+function logError(msg, err) {
+  const dateHumanized = DateTime.now().toLocaleString(DateTime.DATETIME_FULL);
+  const filename = process.env.ERROR_LOG_FILE || DEFAULT_FILENAME_ERRORS;
+  append(filename, `[${dateHumanized}]: ${msg + err}\n`);
+}
+
+/**
+ * Logs the start search time to the file
  * @param {{ from: number, to: number, threads: number}}
  * @returns {DateTime<true>}
  */
-function logStarted({ from, to ,threads }) {
-  const filename = process.env.LOG_FILE || DEFAULT_FILENAME;
+function logStartSeach({ from, to ,threads }) {
+  const filename = process.env.SEARCH_LOG_FILE || DEFAULT_FILENAME_SEARCH;
   const date = DateTime.now();
   const dateHumanized = date.toLocaleString(DateTime.DATETIME_FULL);
-  append(
-    filename,
-    `[${dateHumanized}]: Start scanning.  From: ${from}; to: ${to}; threads: ${threads}\n`
-  );
+  const msg = `[${dateHumanized}]: Start search.  From: ${from}; to: ${to}; threads: ${threads}\n`;
+  append(filename, msg);
   return date;
 }
 
 /**
- * Logs the finish scanning time and duration to the file
+ * Logs the finish search time and duration to the file
  * @param {{ from: number, to: number, threads: number}}
+ * @param {DateTime<true>} startDate
+ */
+function logFinishSearch({ from, to ,threads }, startDate) {
+  const filename = process.env.SEARCH_LOG_FILE || DEFAULT_FILENAME_SEARCH;
+  const date = DateTime.now();
+  const dateHumanized = date.toLocaleString(DateTime.DATETIME_FULL);
+  const duration = date.diff(startDate);
+  const msg = `[${dateHumanized}]: Finish search. From: ${from}; to: ${to}; threads: ${threads}. Duration: ${duration.toHuman()}\n`;
+  append(filename, msg);
+}
+
+/**
+ * Logs the start monitoring time with parameters
+ * @param {number} leftBorder
+ * @param {number} rightBorder
  * @returns {DateTime<true>}
  */
-function logFinished({ from, to ,threads }, startDate) {
+function logStart(leftBorder, rightBorder) {
+  const filename = process.env.LOG_FILE || DEFAULT_FILENAME;
+  const date = DateTime.now();
+  const dateHumanized = date.toLocaleString(DateTime.DATETIME_FULL);
+  const msg = `[${dateHumanized}]: Start scanning. From: ${leftBorder}; to: ${rightBorder}\n`;
+  append(filename, msg);
+  return date;
+}
+
+/**
+ * Logs the finish monitoring time with parameters and duration
+ * @param {number} leftBorder
+ * @param {number} rightBorder
+ * @param {DateTime<true>} startDate
+ */
+function logFinish(leftBorder, rightBorder, startDate) {
   const filename = process.env.LOG_FILE || DEFAULT_FILENAME;
   const date = DateTime.now();
   const dateHumanized = date.toLocaleString(DateTime.DATETIME_FULL);
   const duration = date.diff(startDate);
-  append(
-    filename,
-    `[${dateHumanized}]: Finish scanning. From: ${from}; to: ${to}; threads: ${threads}. Duration: ${duration.toHuman()}\n`
-  );
+  const msg = `[${dateHumanized}]: Finish scanning. From: ${leftBorder}; to: ${rightBorder}. Duration: ${duration.toHuman()}\n`;
+  append(filename, msg);
 }
 
 module.exports = {
-  logStarted,
-  logFinished,
+  logStartSeach,
+  logFinishSearch,
+  logStart,
+  logFinish,
 }
--- src/utils/validate_env.js
+++ src/utils/validate_env.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
 
+const fileNotExistMsg = "[Error] Couldn't find .env file! Use .env.exmaple and fill it with your own parameters.";
+const requiredFieldsMsg = '[Error] Next fields are required in .env file:';
+
 const validateEnv = () => {
   const requiredEnvFields = ['DB_NAME', 'DB_USER', 'DB_HOST', 'DB_PASSWORD', 'DB_PORT'];
 
   if (!fs.existsSync('.env')) {
-    throw Error("[Error] Couldn't find .env file! Use .env.exmaple and fill it with your own parameters.");
+    throw Error(fileNotExistMsg);
   }
   const unexistedFields = requiredEnvFields.filter(field => !process.env[field])
   if (unexistedFields.length > 0) {
-    throw Error(`[Error] Next fields are required in .env file: ${unexistedFields.join(', ')}.`);
+    throw Error(`${requiredFieldsMsg} ${unexistedFields.join(', ')}.`);
   }
 }
 
